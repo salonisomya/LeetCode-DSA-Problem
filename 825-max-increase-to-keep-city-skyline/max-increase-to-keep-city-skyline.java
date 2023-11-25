c7a751f377ff2b96d8ee488c5450df8316003dbf
Somya Saloni <salonisomya2@gmail.com>
2023-11-26 04:59:06 +0530

    Time: 1 ms (51.08%) | Memory: 44 MB (5.22%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -4,14 +4,14 @@ class Solution {
        int n = arr.length;
        int[] row = new int[n];
        int[] col = new int[n];
-
+// below for is to find the max and min 
        for(int i=0;i<n;i++){
            for(int j=0;j<n;j++){
                if(row[i]<arr[i][j]) row[i] = arr[i][j];
                if(col[j]<arr[i][j]) col[j] = arr[i][j];
            }
        }
-
+// below for is to find the new value added to the grid or sum 
        int ans = 0;
        int min = 0;
        for(int i=0;i<n;i++){
