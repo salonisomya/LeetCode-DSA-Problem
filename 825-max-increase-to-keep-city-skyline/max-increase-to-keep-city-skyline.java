b20094dac7ff872eb5c2a3c45ea8c773f2fb39c4
Somya Saloni <salonisomya2@gmail.com>
2023-11-26 04:57:43 +0530

    Time: 1 ms (51.08%) | Memory: 42.8 MB (79.50%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,36 +1,25 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] arr) {
 
-        int n = arr.length;
-        int m = arr[0].length;
-        int row[] = new int[n];
-        int col[] = new int[m];
+       int n = arr.length;
+       int[] row = new int[n];
+       int[] col = new int[n];
 
-        for(int i=0; i<n; i++){
-            int max = Integer.MIN_VALUE;
-            for(int j=0; j<m; j++){
-                max = Math.max(arr[i][j],max);
-            }
-            row[i] = max;
-        }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+               if(row[i]<arr[i][j]) row[i] = arr[i][j];
+               if(col[j]<arr[i][j]) col[j] = arr[i][j];
+           }
+       }
 
-        
-        for(int j=0; j<m; j++){
-            int max = Integer.MIN_VALUE;
-            for(int i=0; i<n; i++){
-                max = Math.max(arr[i][j],max);
-            }
-            col[j] = max;
-        }
-
-        int ans = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                int max = Math.min(row[i],col[j]);
-                if(arr[i][j]<max) ans = ans+max-arr[i][j];
-            }
-        }
-        return ans;
+       int ans = 0;
+       int min = 0;
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+               min = Math.min(row[i],col[j]);
+               if(arr[i][j]<min) ans = ans+(min-arr[i][j]);
+           }
+       }
+       return ans;
     }
 }
