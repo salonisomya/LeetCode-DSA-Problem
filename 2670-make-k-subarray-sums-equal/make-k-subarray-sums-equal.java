a48e05b94d5507d9c39a58b8d7baf56e992dadfa
Somya Saloni <salonisomya2@gmail.com>
2024-03-19 01:01:00 +0530

    Time: 711 ms (5.74%) | Memory: 60.3 MB (41.80%) - LeetSync

--- 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.java
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.java
@@ -1,22 +1,21 @@
 class Solution {
     public long makeSubKSumEqual(int[] arr, int k) {
-       long res = 0;
-       int n = arr.length;
+       long steps =0; 
+        int n = arr.length;
         for(int i=0;i<k;i++){
             List<Integer> list = new ArrayList<>();
             for(int j=i;arr[j]!=0;j=(j+k)%n){
                 list.add(arr[j]);
-                arr[j] = 0;
+                arr[j] =0;
             }
-            Collections.sort(list);
-            for(int val : list){
-                res = res + Math.abs(val-list.get(list.size()/2));
-            }
+            
+             Collections.sort(list);
+             System.out.println(list);
+             for(int val : list){
+                steps = steps + Math.abs(val - list.get(list.size()/2));
+             }
         }
-        return res;
+        return steps;
+
     }
 }
-
-/* cyclic hai toh % n krenge 
-arr[i]==arr[(i+k)%n]
-or agar 1 array mai sare element ko kisi 1 element k equal krna hota hai in min operation mai toh -- mid element k equal kr dete hai-- uske liye Math.abs(val-list.get(list.size()/2)); use krenge */
