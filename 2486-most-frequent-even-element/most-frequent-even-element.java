962b915c68a835911b053278118553559de03256
Somya Saloni <salonisomya2@gmail.com>
2024-03-19 02:09:48 +0530

    Time: 35 ms (25.43%) | Memory: 45.4 MB (9.10%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,19 +1,16 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-       // Map<Integer,Integer> map = new HashMap<>();
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        int ans = -1;
+        Map<Integer, Integer> map = new TreeMap<>();
+        for(int i : nums){
+            if(i%2 == 0){
+                map.put(i,map.getOrDefault(i,0)+1);
             }
         }
         int max = -1;
-        int ans = -1;
-        //for (Map.Entry<Integer, Integer> entry : sorted.entrySet()) 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > max) {
-               // list.add(entry.getKey());
-               max = entry.getValue();
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()>max) {
+                max = entry.getValue();
                 ans = entry.getKey();
             }
         }
