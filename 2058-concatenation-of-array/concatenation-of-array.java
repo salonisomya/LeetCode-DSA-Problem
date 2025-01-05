312bb141c06b1dbadb9d299617dcd7a175da42c9
Somya Saloni <salonisomya2@gmail.com>
2025-01-06 00:54:22 +0530

    Time: 1 ms (96.52%) | Memory: 45.2 MB (69.44%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -3,10 +3,7 @@ class Solution {
         int n = nums.length;
         int ans[] = new int[n+n];
         for (int i=0;i<n;i++){
-            ans[i] = nums[i];
-        }
-        for (int i=0;i<n;i++){
-            ans[n+i] = nums[i];
+            ans[i] = ans [n+i] = nums[i];
         }
         return ans;
     }
