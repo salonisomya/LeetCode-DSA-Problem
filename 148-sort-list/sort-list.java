dc02c7345c5961b6bb08eeaeb4b32d8626377650
Somya Saloni <salonisomya2@gmail.com>
2025-04-06 01:52:35 +0530

    Time: 9 ms (94.68%) | Memory: 57.8 MB (5.29%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,20 +9,47 @@
  * }
  */
 class Solution {
+    static ListNode mid(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    static ListNode merge (ListNode list1 , ListNode list2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(list1 != null && list2 != null){
+            if(list1.val<=list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if(list1 != null){
+            temp.next = list1;
+        }
+        else{
+            temp.next = list2;
+        }
+        return dummy.next;
+    }
     public ListNode sortList(ListNode head) {
-       List<Integer> list = new ArrayList<>();
-       ListNode temp = head;
-       while(temp!= null){
-        list.add(temp.val);
-        temp = temp.next;
-       }
-       Collections.sort(list);
-       temp = head;
-       for(int i=0;i<list.size();i++){
-        temp.val = list.get(i);
-        temp = temp.next;
-       }
-       return head;
-
+        if(head == null || head.next == null) return head;
+        ListNode m = mid(head);
+        ListNode right = m.next;
+        m.next = null;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left,right);
+        
     }
 }
