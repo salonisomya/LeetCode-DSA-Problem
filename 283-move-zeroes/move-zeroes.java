d599312663fe26949964d8bf8e6aa809a07bfbb2
Somya Saloni <salonisomya2@gmail.com>
2025-10-05 19:18:09 +0530

    Time: 2 ms (78.82%) | Memory: 46 MB (86.98%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0; // pointer for position of next non-zero element
-
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                // swap nums[right] with nums[left]
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-
-                left++;
+        int n = nums.length;
+        int t = 0;
+        for(int i=0;i<n;i++){
+            if (nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[t];
+                nums[t] = temp;
+                t++;
             }
         }
     }
