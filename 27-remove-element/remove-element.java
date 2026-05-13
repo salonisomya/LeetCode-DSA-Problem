1ccab398b49bb03d6bb6fe7c8aae61ed47f40171
Somya Saloni <salonisomya2@gmail.com>
2026-05-14 00:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (23.16%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,8 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int n = nums.length;
         int k =0;
-        for(int i=0;i<n;i++){
+        int n = nums.length;
+        for(int i =0;i<n;i++){
             if(nums[i] != val){
                 nums[k] = nums[i];
                 k++;
