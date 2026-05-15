d03b0f77426e38df4ad81c3e30f96b51f7b08e73
Somya Saloni <salonisomya2@gmail.com>
2026-05-16 02:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (76.10%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,31 @@
+
+//Dutch National Flag Algorithm we use in this, 
+//so it basically use when we need to sort the value on basis of group, 
+//it mainly focus on mid where it belong.
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while( mid<= high ){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1) mid ++;
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+            
+        }
     }
+
 }
