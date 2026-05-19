0291288219f5599569fee73522cc752f83dd55da
Somya Saloni <salonisomya2@gmail.com>
2026-05-20 00:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (36.86%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,19 +1,44 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        while(i >= 0 || j >= 0) {
+
+            i = getNextValidCharIndex(s, i);
+            j = getNextValidCharIndex(t, j);
+
+            if(i >= 0 && j >= 0) {
+                if(s.charAt(i) != t.charAt(j)) {
+                    return false;
+                }
+            } else {
+                // one string ended before the other
+                if(i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
     }
 
-    private String build(String str) {
-        StringBuilder sb = new StringBuilder();
+    private int getNextValidCharIndex(String str, int index) {
+        int skip = 0;
 
-        for(char c : str.toCharArray()) {
-            if(c != '#') {
-                sb.append(c);
-            } else if(sb.length() > 0) {
-                sb.deleteCharAt(sb.length() - 1);
+        while(index >= 0) {
+            if(str.charAt(index) == '#') {
+                skip++;
+                index--;
+            } else if(skip > 0) {
+                skip--;
+                index--;
+            } else {
+                break;
             }
         }
 
-        return sb.toString();
+        return index;
     }
 }
